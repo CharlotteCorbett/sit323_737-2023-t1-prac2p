adca4f27bc050e3365f24de2be370be56683c5e6
CharlotteCorbett <charlottec2468@gmail.com>
2025-03-12 13:15:47 +1100

    Added comment documentation to explain what the code written does as part of task requirement

--- Week1/app.js
+++ Week1/app.js
@@ -1,15 +1,24 @@
 
+// Requiring express, which runs as our server.
 const express = require('express');
+
+// Requiring path which finds the path to our index.html file.
 const path = require('path');
 
+// Initiates our app andd calls express into our app.
 const app = express();
 
+// Initiates our port constant, which can be changed here
 const port = 3000;
 
+// Sends request using the path module which finds and gets our 
+// index.html file then sends it to express.
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, './index.html'));
 });
 
+// Tells express to listen on our port variable then output a log to
+// the console/terminal saying what port it's listening to.
 app.listen(port, () => {
     console.log(`Express server listening on port ${port}`);
 });
