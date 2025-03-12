8fad9c67254967d7bd33662b4bc71b4e46eadcc0
CharlotteCorbett <charlottec2468@gmail.com>
2025-03-12 12:57:40 +1100

    Created basic html page and configured express to send the page.

--- Week1/app.js
+++ Week1/app.js
@@ -1,38 +1,16 @@
-// Coming back later (prob wk 2)
 
-console.log("Week-1 ");
+const express = require('express');
+const path = require('path');
 
-let express = require('express');
-var app = express();
+const app = express();
 
-//respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res){
-res.send('hello world');
-})
+const port = 3000;
 
-app.listen(3000)
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, './index.html'));
+});
 
-const express= require("express");
-const res= require("express/ib/response");
-const app= express();
-const addTwoNumber= (n1,n2) => 
-    {
-        return n1+n2;
-    }
-
-app.get("/addTwoNumber", (req,res)=> 
-    {
-        const n1= parseInt(req.query.n1);
-        const n2= parseInt(req.query.n2);
-        const result = addTwoNumber(n1,n2);
-    });
-
-console.log(addTwoNumber(19,12));
-
-const port=3040;
-app.listen(port,()=> 
-{
-    console.log("hello I'm listening to port" + port);
-}
-)
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+});
 
